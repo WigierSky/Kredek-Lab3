f0cffe27dc084fc27d7d330ce3dc403e35f446b3
WigierSky <WigierSky@users.noreply.github.com>
2021-11-25 18:21:29 +0100

    Zmiany

--- Kredek-Lab3/Program.cs
+++ Kredek-Lab3/Program.cs
@@ -13,18 +13,51 @@ namespace Kredek_Lab3
             first_list = word.ToCharArray(0, x);
             int chances = 10;
             char[] second_list = new char[chances];
+            char[] answer = new char[x];
 
-           
-        }
-
-        bool is_Full(char[] x)
-        {
-            if (x != null)
+            Console.WriteLine("\r\n" + "Twoje słowo: " + "\r\n");
+            for (int i = 0; i<x; i++)
             {
-                return true;
+                answer[i] = '#';
+                Console.Write(answer[i]);
             }
-            else return false;
+            while()
+           Console.WriteLine("\r\n" + "Podaj literke:" + "\r\n");
+           char letter = Char.Parse(Console.ReadLine());
+
+            if (word.Contains(letter))
+            {
+                Console.WriteLine("\r\n"+ "Brawo, poprawna literka!" + "\r\n");
+
+                Console.WriteLine("\r\n" + "Twoje słowo: " + "\r\n");
+                for (int i = 0; i < x; i++)
+                {
+                    if (first_list[i] == letter)
+                    {
+                        answer[i] = letter;
+                        Console.Write(answer[i]);
+                        Console.WriteLine();
+                    }
+                    else Console.Write(answer[i]);
+                    Console.WriteLine();
+                }
+            }
+                else
+                {
+                    Console.WriteLine("\r\n"+"Niestety, ta literka nie znajduje w słowie" + "\r\n");
+                    Console.WriteLine("\r\n" + "Twoje słowo: " + "\r\n");
+                    Console.WriteLine("\r\n" + answer[] + "\r\n");
+            }
+
+
+
+               
+            
+
+
+
 
         }
+
     }
 }
