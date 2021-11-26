fbca0e4a68fdb0bfe1efb9978c4f37528d87fbc9
WigierSky <WigierSky@users.noreply.github.com>
2021-11-26 11:35:59 +0100

    Final?

--- Kredek-Lab3/Program.cs
+++ Kredek-Lab3/Program.cs
@@ -26,124 +26,144 @@ namespace Kredek_Lab3
             Console.WriteLine("\r\n");
             bool c = true;
             int k = 0;
+            bool d = true;
             Console.WriteLine("Masz tylko 5 szans! Inaczej bałwan zawiśnie!");
             while (c && second_list[chances - 1] == 0)
             {
 
                 Console.WriteLine("\r\n" + "Podaj literke:");
                 char letter = Char.Parse(Console.ReadLine());
-   
 
-                if (word.Contains(letter))
+                for (int q = 0; q < chances; q++)
                 {
-                    Console.WriteLine("\r\n" + "Brawo, poprawna literka!" + "\r\n");
-
-                    Console.WriteLine("\r\n" + "Twoje słowo: ");
-                    Console.Write("=================" + "\r\n");
-                    for (int i = 0; i < x; i++)
+                    if (second_list[q] == letter)
                     {
-                        if (first_list[i] == letter)
+                        d = true;
+                        break;
+                    }
+                    else d = false;
+                }
+
+
+                if (d == false)
+                {
+                    if (word.Contains(letter))
+                    {
+                        Console.WriteLine("\r\n" + "Brawo, poprawna literka!" + "\r\n");
+
+                        Console.WriteLine("\r\n" + "Twoje słowo: ");
+                        Console.Write("=================" + "\r\n");
+                        for (int i = 0; i < x; i++)
                         {
-                            answer[i] = letter;
+                            if (first_list[i] == letter)
+                            {
+                                answer[i] = letter;
+                                Console.Write(answer[i]);
+                            }
+                            else Console.Write(answer[i]);
+                        }
+                        Console.Write("\r\n" + "=================");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        second_list[k] = letter;
+                        k++;
+                        Console.WriteLine("\r\n" + "Niestety, ta literka nie znajduje w słowie" + "\r\n");
+                        Console.WriteLine("\r\n" + "Twoje słowo: ");
+                        Console.Write("================" + "\r\n");
+                        for (int i = 0; i < x; i++)
+                        {
                             Console.Write(answer[i]);
                         }
-                        else Console.Write(answer[i]);
-                    }
-                    Console.Write("\r\n" + "=================");
-                    Console.WriteLine();
-                }
-                else
-                {
-                    second_list[k] = letter;
-                    k++;
-                    Console.WriteLine("\r\n" + "Niestety, ta literka nie znajduje w słowie" + "\r\n");
-                    Console.WriteLine("\r\n" + "Twoje słowo: ");
-                    Console.Write("================" + "\r\n");
-                    for (int i = 0; i < x; i++)
-                    {
-                        Console.Write(answer[i]);
-                    }
-                    Console.Write("\r\n" + "=================");
-                    Console.WriteLine();
+                        Console.Write("\r\n" + "=================");
+                        Console.WriteLine();
 
-                    if(k==1)
+                        if (k == 1)
+                        {
+                            Console.WriteLine("===============");
+                            Console.WriteLine("___________");
+                            Console.WriteLine("          |");
+                            Console.WriteLine();
+                            Console.WriteLine("===============");
+                        }
+                        else
+                            if (k == 2)
+                        {
+                            Console.WriteLine("===============");
+                            Console.WriteLine("___________");
+                            Console.WriteLine("          |");
+                            Console.WriteLine("         (..) ");
+                            Console.WriteLine();
+                            Console.WriteLine("===============");
+                        }
+                        else
+                            if (k == 3)
+                        {
+                            Console.WriteLine("===============");
+                            Console.WriteLine("___________");
+                            Console.WriteLine("          |");
+                            Console.WriteLine("      \\ (..) /");
+                            Console.WriteLine();
+                            Console.WriteLine("===============");
+                        }
+                        else
+                            if (k == 4)
+                        {
+                            Console.WriteLine("===============");
+                            Console.WriteLine("___________");
+                            Console.WriteLine("          |");
+                            Console.WriteLine("      \\ (..) /");
+                            Console.WriteLine("       (    )");
+                            Console.WriteLine();
+                            Console.WriteLine("===============");
+                        }
+
+
+                    }
+
+                    for (int j = 0; j < x; j++)
                     {
-                        Console.WriteLine("===============");
-                        Console.WriteLine("___________");
-                        Console.WriteLine("          |");
+                        if (answer[j] == first_list[j])
+                        {
+                            c = false;
+                        }
+                        else
+                        {
+                            c = true;
+                            break;
+                        }
+
+                    }
+
+
+
+                    if (c == false)
+                    {
+                        Console.WriteLine("____________________________");
+                        Console.WriteLine("\r\n" + "Gratulacje! Odgadłeś słowo! :>");
+                        Console.WriteLine("____________________________");
+                    }
+                    else
+                    if(k==5)
+                    {
+                        Console.WriteLine("____________________________");
+                        Console.WriteLine("\r\n" + "Przykro mi, przegrałeś :<");
+                        Console.WriteLine("____________________________");
                         Console.WriteLine();
-                        Console.WriteLine("===============");
-                    }else
-                        if(k==2)
-                    {
-                        Console.WriteLine("===============");
-                        Console.WriteLine("___________");
-                        Console.WriteLine("          |");
-                        Console.WriteLine("         (..) ");
-                        Console.WriteLine();
-                        Console.WriteLine("===============");
-                    }else
-                        if(k==3)
-                    {
-                        Console.WriteLine("===============");
-                        Console.WriteLine("___________");
-                        Console.WriteLine("          |");
-                        Console.WriteLine("      \\ (..) /");
-                        Console.WriteLine();
-                        Console.WriteLine("===============");
-                    }else
-                        if(k==4)
-                    {
                         Console.WriteLine("===============");
                         Console.WriteLine("___________");
                         Console.WriteLine("          |");
                         Console.WriteLine("      \\ (..) /");
                         Console.WriteLine("       (    )");
+                        Console.WriteLine("      (      )");
                         Console.WriteLine();
                         Console.WriteLine("===============");
                     }
-                        
-
                 }
-
-                for (int j = 0; j < x; j++)
-                {
-                    if (answer[j] == first_list[j])
-                    {
-                        c = false;
-                    }
-                    else
-                    {
-                        c = true;
-                        break;
-                    }
-
-                }
+                else Console.WriteLine("\r\n"+"Już podałeś tą literkę!");
 
             }
-
-            if (c == false)
-            {
-                Console.WriteLine("____________________________");
-                Console.WriteLine("\r\n" + "Gratulacje! Odgadłeś słowo! :>");
-                Console.WriteLine("____________________________");
-            }
-            else
-            {
-                Console.WriteLine("____________________________");
-                Console.WriteLine("\r\n" + "Przykro mi, przegrałeś :<");
-                Console.WriteLine("____________________________");
-                Console.WriteLine();
-                Console.WriteLine("===============");
-                Console.WriteLine("___________");
-                Console.WriteLine("          |");
-                Console.WriteLine("      \\ (..) /");
-                Console.WriteLine("       (    )");
-                Console.WriteLine("      (      )");
-                Console.WriteLine();
-                Console.WriteLine("===============");
-            }
-
         }
     }
 }
